194361b60cac13a265028b96f39973fae0883092
Higor Gonçalves Brandão <higorb2000@gmail.com>
2021-07-19 23:56:51 -0300

    first commit

---
+++ src/pages/home/index.js
@@ -0,0 +1,18 @@
+import { React } from 'react';
+
+function Home() {
+
+    return (
+
+        <div className="App">
+
+            <h1>Olá, mundo!</h1>
+
+        </div>
+
+    )
+
+
+}
+
+export default Home;
