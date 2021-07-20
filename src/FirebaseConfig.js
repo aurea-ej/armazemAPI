e422c9d4620246a08bf554fd788c60e535f02501
Higor Gonçalves Brandão <higorb2000@gmail.com>
2021-07-20 01:22:48 -0300

    add firebase

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWvZFi1AHS49xoF-x0Vuf9p3BOvEoRAXA",
+    authDomain: "armazem-do-vinho.firebaseapp.com",
+    databaseURL: "https://armazem-do-vinho-default-rtdb.firebaseio.com",
+    projectId: "armazem-do-vinho",
+    storageBucket: "armazem-do-vinho.appspot.com",
+    messagingSenderId: "627040949952",
+    appId: "1:627040949952:web:3c6c25b2912573b4697f25",
+};
+
+export default firebaseConfig;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,40 @@
 import { React } from 'react';
+import { useState, useEffect } from 'react';
+
+import './style.scss'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import firebaseConfig from '../../FirebaseConfig.js'
 
 function Home() {
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+
+        if (!firebase.apps.length)
+            firebase.initializeApp(firebaseConfig);
+
+        var firebaseRef = firebase.database().ref('items/');
+
+        firebaseRef.on('value', (snapshot) => {
+
+            if (snapshot.exists()) {
+
+                var data = snapshot.val()
+                var temp = Object.keys(data).map((key) => data[key])
+                setData(temp)
+
+            }
+            else {
+                console.log("No data available");
+            }
+
+        });
+
+    }, [])
 
     return (
 
@@ -8,10 +42,27 @@ function Home() {
 
             <h1>Olá, mundo!</h1>
 
+            {
+                data.map((item) => {
+
+                    return (
+
+                        <div className="list">
+
+                            <p>Nome: {item.name}</p>
+                            <p>Descrição: {item.description}</p>
+                            <p>Valor: R${item.value}</p>
+
+                        </div>
+
+                    )
+
+                })
+            }
+
         </div>
 
     )
-
 
 }
 
