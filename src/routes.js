54276c710fa0a416c9e7880aeda7857ee18e04de
Micaella Barcellos Pereira <micaellab30@gmail.com>
2021-07-21 18:40:30 -0300

    pág sobre nós

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 import { React } from 'react'
-import { Route, BrowserRouter} from 'react-router-dom'
+import { Route, BrowserRouter } from 'react-router-dom'
 
 import Home from './pages/home'
 import Contact from './pages/contact'
+import About from './pages/about'
 
 const Routes = () => {
 
@@ -11,12 +12,12 @@ const Routes = () => {
         <BrowserRouter>
 
             <Route component={Home} path='/' exact />
+            <Route component={About} path="/quemsomos" />
             <Route component={Contact} path='/contato'/>
-        
+
         </BrowserRouter>
 
     )
 
 }
-
 export default Routes;
