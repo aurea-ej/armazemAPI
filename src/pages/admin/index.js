c628bd537ed851db9dfbf20d8baf20f1775efba7
Guilherme Barreto <48062197+BarretoNV@users.noreply.github.com>
2021-07-24 18:24:11 -0300

    pagina de admin frontend feita

---
+++ src/pages/admin/index.js
@@ -0,0 +1,196 @@
+import { useEffect, useState } from 'react'
+import Header from '../../components/header'
+import Footer from '../../components/footer'
+import { Link } from 'react-router-dom'
+import './style.scss'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+function Admin() {
+
+    const [loginData,setLoginData] = useState({
+
+        email: '',
+        password: ''
+
+    })
+    const [userIsLogged, setUserIsLogged] = useState(false);
+
+
+    function makeLogin () {
+
+        firebase.auth().signInWithEmailAndPassword(loginData.email, loginData.password)
+        .then(() => {
+
+            var userEmail = localStorage.getItem('userEmail')
+        
+            firebase.database().ref('admins').get('/admins')
+            .then(function (snapshot) {
+
+                if (snapshot.exists()) {
+
+                    var data = snapshot.val()
+                    var temp = Object.keys(data).map((key) => data[key])
+
+                    temp.map((item) => {
+
+                        if(item.email == userEmail)
+                            setUserIsLogged(true)
+
+                    })
+                }
+                else {
+                    console.log("No data available");
+                }
+            })
+            
+            
+            localStorage.setItem('userEmail',loginData.email)
+
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(errorMessage)
+        }); 
+        
+    }
+
+    function handleInputLoginChange(event) {
+
+        const {name, value} = event.target
+
+        setLoginData ({
+
+            ...loginData, [name]: value
+
+        })
+        
+    }
+
+/*     useEffect(() => {
+        
+        window.scrollTo(0, 0);
+
+        if(!firebase.apps.length)
+            firebase.initializeApp(firebaseConfig)
+
+    }, []); */
+
+    useEffect(() => {
+
+        var userEmail = localStorage.getItem('userEmail')
+        
+        firebase.database().ref('admins').get('/admins')
+        .then(function (snapshot) {
+
+            if (snapshot.exists()) {
+
+                var data = snapshot.val()
+                var temp = Object.keys(data).map((key) => data[key])
+
+                temp.map((item) => {
+
+                    if(item.email == userEmail)
+                        setUserIsLogged(true)
+
+                })
+            }
+            else {
+                console.log("No data available");
+            }
+        })
+
+    }, []);
+
+    if (userIsLogged) {
+
+        return (
+
+            <div className='Admin'>
+
+                <Header />
+
+                <div id='mainAdmin' >
+
+                    <div className='titleAdmin' >
+                        <h1>Bem vindos, equipe Armazém do Vinho 🍷</h1>
+                    </div>
+
+                    <div className='titleAdmin' >
+                        <h3>O que deseja fazer?</h3>
+                    </div>
+
+                    <div className='optionAdminPage' >
+
+                        <ul>
+
+                            <Link to="/Pedidos" >Pedidos em andamento</Link>
+                            <Link to="/AdminItems" >Cadastro/alteração de itens</Link>
+                            <Link to="/AdminVendedor" >Cadastro/alteração de vendedores</Link>
+                            <Link to="/AdminFornecedor" >Cadastro/alteração de fornecedores </Link>
+                            <Link to='/AdminProdutoFornecedor' >Cadastro/alteração de produtos dos fornecedores</Link>
+                            <Link to='/PedidoFornecedor' >Realizar pedido do fornecedor</Link>
+                            <Link to="/ListaDeClientes" >Listagem de clientes</Link>
+                            <Link to="/relatorios" >Relatórios</Link>
+                            <Link to="/Estoque" >Estoque</Link>
+                            
+                        </ul>
+
+                    </div>
+
+                </div>
+
+                <Footer />
+            </div>
+
+        )
+        
+    } else {
+
+        return (
+
+            <div className='Admin'>
+
+                <Header />
+
+                    <main id='mainRegister'> 
+
+                        <div className='adminRegister'>
+
+                            <div className='titleAdmin' >
+                                <h1>Bem vindos, equipe Armazém do Vinho 🍷</h1>
+                            </div>
+
+                            <fieldset>
+
+
+                                <h1>Entrar</h1>
+
+                                <input name='email' onChange={handleInputLoginChange} placeholder='E-mail' />
+
+                                <input name='password' type='password' onChange={handleInputLoginChange} placeholder='Senha' />
+
+                            </fieldset>
+
+                            <div className='buttonsFormRegister' >
+
+                                <Link id='enterButtonSignIn' onClick={makeLogin}>Entrar</Link>
+
+                            </div>
+
+                        </div>
+
+                    </main>
+
+                <Footer />
+            </div>
+
+        )
+    
+    }
+    
+}
+
+export default Admin
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Home from './pages/home'
 import Contact from './pages/contact'
 import About from './pages/about'
 import Register from './pages/signIn'
+import Admin from './pages/admin'
 
 const Routes = () => {
 
@@ -16,6 +17,7 @@ const Routes = () => {
             <Route component={About} path='/quemsomos' />
             <Route component={Contact} path='/contato'/>
             <Route component={Register} path='/entrar'/>
+            <Route component={Admin} path='/admin'/>
 
         </BrowserRouter>
 
